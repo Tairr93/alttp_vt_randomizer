961e846a85f0deb40e598c9c052c39b39b0481c0
Tairr93 <42362669+Tairr93@users.noreply.github.com>
2018-08-14 04:07:31 +0200

    about.php

---
+++ resources/lang/de/about.php
@@ -0,0 +1,7 @@
+<?php
+return [
+	'content' => [
+		'<span class="font-weight-bold">ALttP: Randomizer</span> ist ein neuer Ansatz von dem klassischen Spiel The Legend of Zelda: A Link to the Past. Jedes Durchspielen mischt die Fundorte der wichtigen Gegenstände im Spiel. Wirst du den Bogen auf dem Gipfel vom Todesberg finden, der Feuerstab, der still in der Bibliothekt ruht, oder selbst das Master-Schwert das im Hühnerstall wartet?',
+		'Fordere deine Freunde heraus um die schnellste Zeit in einer bestimmten Mischung zu kriegen oder macht mit beim <a href="/de/races">Wöchentlichen Speedrun Wettbewerb</a>. Wenn du dein Geschickt gut genug ist, wirst du villeicht die Krone nach Hause holen in einem unserer <a href="/en/races">zweimal jährlichen Einladungsturniere</a>. Man sieht sich in Hyrule!',
+	],
+];
